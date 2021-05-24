855cd8197af976c2e243950e36dda2bfe2aeeb3b
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-24 16:05:10 -0300

    Update ejercicio_5.py

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -12,12 +12,18 @@ import random
 
 # --------------------------------
 # Aquí dentro definir la función contar
-
-
+def contar_cantidad_de_3(lista_numeros):
+    cantidad_de_tres = (lista_numeros).count(3)
+    return cantidad_de_tres
 
 # Aquí copiar la función lista_aleatoria
 # ya elaborada
-
+def lista_aleatoria(inicio, fin, cantidad):
+    lista=[]
+    for i in range(cantidad):
+        numero = random.randrange(inicio, fin+1)
+        lista.append(numero)
+    return lista
 
 # --------------------------------
 
@@ -36,7 +42,7 @@ if __name__ == '__main__':
     # un rango de 1 a 9 inclusive
 
     # lista_numeros = lista_aleatoria(inicio, fin, cantidad)
-
+    lista_numeros = lista_aleatoria(inicio, fin, cantidad)
     # Generar una una nueva funcion que se llame "contar",
     #que cuenta la cantidad de veces que un elemento pasado
     # por parámetro se repite en la lista también pasada por parámetro
@@ -48,15 +54,15 @@ if __name__ == '__main__':
     
     # Luego quiero averiguar cuantas veces se repite el numero 3
     # cantidad_tres = contar(lista_numeros, 3)
-
+    cantidad_tres = contar_cantidad_de_3(lista_numeros)
     # Luego de crear la función invocarla en este lugar:
     # Averiguar cuantas veces se repite el numero 3
-
+    
     # cantidad_tres = contar(lista_numeros, 3)
 
     # Imprimir en pantalla "cantidad_tres" que informa
     # cuantas veces se repite el tres en la lista
 
     # print(cantidad_tres)
-
+    print("cantidad_tres", cantidad_tres)
     print("terminamos")
